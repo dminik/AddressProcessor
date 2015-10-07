15982ccf2aff29b85ea357a2671e3d26f8af3842
Nikulin Dmitriy <dmin@ciklum.com>
2015-10-07 17:22:50 +0300

    Поправил build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 nuget restore src\Exercise.sln
-msbuild src\AddressProcessor\Helpers.Common.csproj
-msbuild src\AddressProcessor\DataProviders.csproj
-msbuild src\AddressProcessor.Tests\AddressProcessing.csproj
+msbuild src\Helpers.Common\Helpers.Common.csproj
+msbuild src\DataProviders\DataProviders.csproj
+msbuild src\AddressProcessor\AddressProcessing.csproj
 msbuild src\AddressProcessor.Tests\AddressProcessing.Tests.csproj
